1f0d847e8ee49ba205aa05f9628fba046cf8815f
SaiBlaze <84612731+SaiBlaze@users.noreply.github.com>
2021-06-07 14:34:54 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("C U T N I O F N",700,150)
+    fill("blue")
+    text("Hint:To perform a spesific task",700,170)
+
+    fill("white")
+    textSize(15)
+    text("A A T D A B S E",100,250)
+    fill("blue")
+    text("Hint:To store values",100,270)
+
+}
